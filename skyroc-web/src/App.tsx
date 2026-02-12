c4adcd0924138dc0b169b02a5ab631167b803de8
aimhigh-aa <1254463211@qq.com>
2026-02-12 18:19:45 +0800

    feat:elegantrouter

---
+++ skyroc-web/src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import { RouterProvider } from "@/features/router";
+
+import "./App.css";
+
+function App() {
+  return <RouterProvider />;
+}
+
+export default App;
---
+++ skyroc-web/src/pages/demo.jsx
@@ -0,0 +1,3 @@
+export default function DemoPage() {
+  return <div>这是自动生成的 Demo 页面</div>;
+}
